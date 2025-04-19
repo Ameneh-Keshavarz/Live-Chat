2251512088c4c92716e8a9d6cea6917f60e7ab99
ameneh-keshavarz <amy.cyf2024@gmail.com>
2025-04-19 21:10:40 +0100

    Add messages to show server status

--- WebSocket/backend/server.js
+++ WebSocket/backend/server.js
@@ -2,11 +2,15 @@ const http = require('http');
 const WebSocketServer = require('websocket').server;
 
 const server = http.createServer();
-server.listen(3000);
+server.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
 
 const wsServer = new WebSocketServer({ httpServer: server });
 
 wsServer.on('request', function(request) {
+  console.log('Received a WebSocket request');
   const connection = request.accept(null, request.origin);
+  console.log('Client connected');
   connection.sendUTF('Hello from server!');
 });
