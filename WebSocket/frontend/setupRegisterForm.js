dab9837efc9315c8590042a4b0362762c16f0e60
ameneh-keshavarz <amy.cyf2024@gmail.com>
2025-05-10 19:03:06 +0100

    seperating register from auth.js

---
+++ WebSocket/frontend/setupRegisterForm.js
@@ -0,0 +1,25 @@
+const API_URL = "http://localhost:3000/api/auth";
+
+const registerForm = document.getElementById("register-form");
+
+registerForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const username = document.getElementById("register-username").value.trim();
+    const password = document.getElementById("register-password").value;
+  
+    const res = await fetch(`${API_URL}/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+    });
+    if (!res.ok) {
+        const err = await res.json();
+        alert(err.error || "Registration failed.");
+        return;
+    }
+  
+    alert("Registration successful! Please log in.");
+    window.location.href = "login.html";
+});
+  
+  
