37f98f098c1fa202e95e1753cbcee0db1f032a99
ameneh-keshavarz <amy.cyf2024@gmail.com>
2025-05-12 09:18:30 +0100

    add full_name in registration

--- WebSocket/frontend/setupRegisterForm.js
+++ WebSocket/frontend/setupRegisterForm.js
@@ -4,22 +4,23 @@ const registerForm = document.getElementById("register-form");
 
 registerForm.addEventListener("submit", async (e) => {
     e.preventDefault();
+
+    const full_name = document.getElementById("register-fullname").value.trim();
     const username = document.getElementById("register-username").value.trim();
     const password = document.getElementById("register-password").value;
-  
+
     const res = await fetch(`${API_URL}/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify({ full_name, username, password }),
     });
+
     if (!res.ok) {
         const err = await res.json();
         alert(err.error || "Registration failed.");
         return;
     }
-  
+
     alert("Registration successful! Please log in.");
     window.location.href = "login.html";
 });
-  
-  
