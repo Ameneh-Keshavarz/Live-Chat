cd7f4a16419df794958f9110241125e4799a03d2
ameneh-keshavarz <amy.cyf2024@gmail.com>
2025-05-14 15:06:36 +0100

    enhance error handling

--- WebSocket/backend/controllers/loginController.js
+++ WebSocket/backend/controllers/loginController.js
@@ -2,21 +2,36 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { db } from "../db/db.js";
 import { asyncWrapper } from "../middlewares/asyncWrapper.js";
-import dotenv from "dotenv";
 import { ApiError } from "../errors/ApiError.js";
 import { StatusCodes } from "http-status-codes";
+import { JWT_SECRET } from "../config.js";
 
-dotenv.config();
 
-const JWT_SECRET = process.env.JWT_SECRET;
+//const JWT_SECRET = process.env.JWT_SECRET;
 
 export const login = asyncWrapper(async (req, res) => {
   const { username, password } = req.body;
-  const user = await db("users").where({ username }).first();
+  console.log("Login attempt:", username);
 
-  if (!user || !(await bcrypt.compare(password, user.password))) {
+  const user = await db("users").where({ username }).first();
+  if (!user) {
+    console.log("User not found");
     throw new ApiError("Invalid credentials", StatusCodes.UNAUTHORIZED);
   }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    console.log("Password mismatch");
+    throw new ApiError("Invalid credentials", StatusCodes.UNAUTHORIZED);
+  }
+
+  console.log("Login success");
+
+  if (!JWT_SECRET) {
+    console.error("JWT_SECRET is not defined!");
+    throw new Error("Missing JWT_SECRET");
+  }
+  
 
   const token = jwt.sign({ userId: user.id, username: user.username }, JWT_SECRET, {
     expiresIn: "1h",
@@ -26,6 +41,7 @@ export const login = asyncWrapper(async (req, res) => {
     token,
     userId: user.id,
     username: user.username,
-    full_name: user.full_name, 
+    full_name: user.full_name,
   });
 });
+
