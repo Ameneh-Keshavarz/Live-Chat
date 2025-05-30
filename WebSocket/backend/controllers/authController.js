aea71ae06be9a33b3ce544a8b554b4dfdfbe3e74
ameneh-keshavarz <amy.cyf2024@gmail.com>
2025-05-30 12:31:05 +0100

    move asyncWrapper usage from authController to authRoutes

--- WebSocket/backend/controllers/authController.js
+++ WebSocket/backend/controllers/authController.js
@@ -1,13 +1,12 @@
-import { asyncWrapper } from "../middlewares/asyncWrapper.js";
 import { StatusCodes } from "http-status-codes";
 import { registerUser, loginUser } from "../services/authService.js";
 
-export const register = asyncWrapper(async (req, res) => {
+export const register = async (req, res) => {
   await registerUser(req.body);
   res.status(StatusCodes.CREATED).json({ message: "User registered" });
-});
+};
 
-export const login = asyncWrapper(async (req, res) => {
+export const login = async (req, res) => {
   const { token, user } = await loginUser(req.body);
 
   res
@@ -18,4 +17,4 @@ export const login = asyncWrapper(async (req, res) => {
       maxAge: 60 * 60 * 1000,
     })
     .json(user);
-});
+};
--- WebSocket/backend/routes/authRoutes.js
+++ WebSocket/backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
+import { asyncWrapper } from "../middlewares/asyncWrapper.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", asyncWrapper(register));
+router.post("/login", asyncWrapper(login));
 
 export default router;
