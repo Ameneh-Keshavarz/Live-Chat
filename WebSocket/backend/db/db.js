4f59f7d6007555625333efbaf7db031ec041bb3c
ameneh-keshavarz <amy.cyf2024@gmail.com>
2025-06-26 15:49:23 +0100

    enable foreign key constraints for each DB connection using afterCreate hook

--- WebSocket/backend/db/db.js
+++ WebSocket/backend/db/db.js
@@ -1,9 +1,15 @@
 import knex from "knex";
+import sqlite3 from "sqlite3";
 
 export const db = knex({
   client: "sqlite3",
   connection: {
-    filename: "./chat.db"
+    filename: "./chat.db",
   },
   useNullAsDefault: true,
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done);
+    },
+  },
 });
